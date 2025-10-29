b3d06bb78ab45c8a9e57127fe6d52fde490d5941
Kevin23221 <guacanamekevin2007@gmail.com>
2025-10-28 19:09:53 -0500

    Primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './page/home/home';
+import { About} from './page/about/about';
+import { Products } from './page/products/products';
+import { Contact } from './page/contact/contact';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+
+    {path:'',component:Home},
+    {path:'nosotros',component:About},
+    {path:'producto',component:Products},
+    {path:'contacto',component:Contact},
+    {path:'**',component:Home}
+
+];
